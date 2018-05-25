a92b1cc00c3f7a44427363313644f1a979b8ee89
justinl <justinl@hollard.co.za>
2018-05-25 16:49:40 +0200

    Updated search component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
 
-const API_KEY = '';
+const API_KEY = 'AIzaSyBYwXYPC4BAZiRaFWNWeJ8GnVQA1wUiQlQ';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });            
+        });        
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
