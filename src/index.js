d19fad4a665cdf58b4a8510be60d27cd1cd0a3b5
justinl <justinl@hollard.co.za>
2018-05-25 10:51:02 +0200

    Commit without API key

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar'
 
-const API_KEY = 'AIzaSyBYwXYPC4BAZiRaFWNWeJ8GnVQA1wUiQlQ';
+const API_KEY = '';
 
 const App = () => {
     return (
