e16608c083748b0babd06c5c2380c402f3f2ce2e
justinl <justinl@hollard.co.za>
2018-06-01 16:43:57 +0200

    Implemented styling; the ability to view videos; as well as to select relevant videos

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,8 +9,7 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <div>
-                Search
+            <div className="search-bar">                
                 <input 
                     value={this.state.term}
                     onChange={event => this.setState({ term: event.target.value })} />
